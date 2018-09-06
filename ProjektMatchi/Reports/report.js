ac37246d2f9823a1036e0f98f380c11dfa578876
Marcu <Marcu@DESKTOP-C8K9OFL>
2018-09-06 10:43:38 +0200

    sadh

--- ProjektMatchi/Reports/report.js
+++ ProjektMatchi/Reports/report.js
@@ -1,4 +1,104 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("ProjektMatchi/ProjektMatchi/BookACourt.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("ProjektMatchi/ProjektMatchi/BookByTheWeek.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Book courts by week schedual",
+  "description": "",
+  "id": "book-courts-by-week-schedual",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "I book a court in the week view, at a later date",
+  "description": "",
+  "id": "book-courts-by-week-schedual;i-book-a-court-in-the-week-view,-at-a-later-date",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "I am logged in to Matchi",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "have chosen a sport hall",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "I search for available courts",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "Click visa vecka",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I click a time later in the week",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I click book",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I can book a court",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_am_logged_in_to_Matchi()"
+});
+formatter.result({
+  "duration": 15937040361,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.have_chosen_a_sport_hall()"
+});
+formatter.result({
+  "duration": 3965219582,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_search_for_available_courts()"
+});
+formatter.result({
+  "duration": 64018,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.click_visa_vecka()"
+});
+formatter.result({
+  "duration": 1112657792,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_book_a_court_later_in_the_week()"
+});
+formatter.result({
+  "duration": 1154084546,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_click_book()"
+});
+formatter.result({
+  "duration": 1975267058,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_can_book_a_court()"
+});
+formatter.result({
+  "duration": 9521540065,
+  "status": "passed"
+});
+formatter.uri("ProjektMatchi/ProjektMatchi/Bookingcourts.feature");
 formatter.feature({
   "line": 1,
   "name": "As customer I want to be able to book one or several courts.",
@@ -48,42 +148,42 @@ formatter.match({
   "location": "MatchiTestSteps.i_am_logged_in_to_Matchi()"
 });
 formatter.result({
-  "duration": 15369717473,
+  "duration": 13802916575,
   "status": "passed"
 });
 formatter.match({
   "location": "MatchiTestSteps.have_chosen_a_sport_hall()"
 });
 formatter.result({
-  "duration": 4643608030,
+  "duration": 5284535941,
   "status": "passed"
 });
 formatter.match({
   "location": "MatchiTestSteps.i_search_for_available_courts()"
 });
 formatter.result({
-  "duration": 34061,
+  "duration": 59093,
   "status": "passed"
 });
 formatter.match({
   "location": "MatchiTestSteps.chosen_a_specific_time()"
 });
 formatter.result({
-  "duration": 346479505,
+  "duration": 384647520,
   "status": "passed"
 });
 formatter.match({
   "location": "MatchiTestSteps.i_click_book()"
 });
 formatter.result({
-  "duration": 1690217746,
+  "duration": 1786898230,
   "status": "passed"
 });
 formatter.match({
   "location": "MatchiTestSteps.i_can_book_a_court()"
 });
 formatter.result({
-  "duration": 9919466307,
+  "duration": 9178840469,
   "status": "passed"
 });
 formatter.scenario({
@@ -133,46 +233,50 @@ formatter.match({
   "location": "MatchiTestSteps.i_am_logged_in_to_Matchi()"
 });
 formatter.result({
-  "duration": 13057289159,
+  "duration": 13365186178,
   "status": "passed"
 });
 formatter.match({
   "location": "MatchiTestSteps.have_chosen_a_sport_hall()"
 });
 formatter.result({
-  "duration": 4466580572,
+  "duration": 4741414978,
   "status": "passed"
 });
 formatter.match({
   "location": "MatchiTestSteps.i_search_for_available_courts()"
 });
 formatter.result({
-  "duration": 58273,
+  "duration": 42679,
   "status": "passed"
 });
 formatter.match({
-  "location": "MatchiTestSteps.click_on_the_button(String)"
+  "location": "MatchiTestSteps.click_on_the_button()"
 });
 formatter.result({
-  "duration": 1815883,
-  "error_message": "cucumber.runtime.CucumberException: Arity mismatch: Step Definition \u0027ProjektMatchi.ProjektMatchi.MatchiTestSteps.click_on_the_button(String) in file:/C:/Users/Marcu/git/Matchi/ProjektMatchi/target/classes/\u0027 with pattern [^click on the Boka flera button$] is declared with 1 parameters. However, the gherkin step has 0 arguments []. \nStep: And click on the Boka flera button\r\n\tat cucumber.runtime.StepDefinitionMatch.arityMismatch(StepDefinitionMatch.java:102)\r\n\tat cucumber.runtime.StepDefinitionMatch.transformedArgs(StepDefinitionMatch.java:60)\r\n\tat cucumber.runtime.StepDefinitionMatch.runStep(StepDefinitionMatch.java:37)\r\n\tat cucumber.runtime.Runtime.runStep(Runtime.java:299)\r\n\tat cucumber.runtime.model.StepContainer.runStep(StepContainer.java:44)\r\n\tat cucumber.runtime.model.StepContainer.runSteps(StepContainer.java:39)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:44)\r\n\tat cucumber.runtime.junit.ExecutionUnitRunner.run(ExecutionUnitRunner.java:91)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:63)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:18)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\r\n\tat cucumber.runtime.junit.FeatureRunner.run(FeatureRunner.java:70)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:93)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:37)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\r\n\tat cucumber.api.junit.Cucumber.run(Cucumber.java:98)\r\n\tat org.eclipse.jdt.internal.junit4.runner.JUnit4TestReference.run(JUnit4TestReference.java:86)\r\n\tat org.eclipse.jdt.internal.junit.runner.TestExecution.run(TestExecution.java:38)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.runTests(RemoteTestRunner.java:538)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.runTests(RemoteTestRunner.java:760)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.run(RemoteTestRunner.java:460)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.main(RemoteTestRunner.java:206)\r\n",
-  "status": "failed"
+  "duration": 493280002,
+  "status": "passed"
 });
-formatter.match({});
+formatter.match({
+  "location": "MatchiTestSteps.choose_three_court_times()"
+});
 formatter.result({
-  "status": "undefined"
+  "duration": 1106000786,
+  "status": "passed"
 });
 formatter.match({
   "location": "MatchiTestSteps.i_click_book()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 1789549624,
+  "status": "passed"
 });
 formatter.match({
   "location": "MatchiTestSteps.i_can_book_more_than_one_court()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 25835111667,
+  "status": "passed"
 });
 formatter.uri("ProjektMatchi/ProjektMatchi/LogInToMatchi.feature");
 formatter.feature({
@@ -217,4 +321,321 @@ formatter.step({
 });
 formatter.match({
   "location": "MatchiTestSteps.i_am_on_the_Machi_site()"
+});
+formatter.result({
+  "duration": 8662066429,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_have_not_logged_in()"
+});
+formatter.result({
+  "duration": 41857,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "mjukvarutestare4@mailinator.com",
+      "offset": 16
+    },
+    {
+      "val": "mjukvarutestare",
+      "offset": 63
+    }
+  ],
+  "location": "MatchiTestSteps.i_use_username_and_password(String,String)"
+});
+formatter.result({
+  "duration": 2890519696,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_click_the_log_in_button()"
+});
+formatter.result({
+  "duration": 2217604750,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_am_logged_in()"
+});
+formatter.result({
+  "duration": 1365575208,
+  "status": "passed"
+});
+formatter.uri("ProjektMatchi/ProjektMatchi/PaymentMethodMatchi.feature");
+formatter.feature({
+  "line": 1,
+  "name": "As a customer I want to be able choose different payment methods for my booking.",
+  "description": "",
+  "id": "as-a-customer-i-want-to-be-able-choose-different-payment-methods-for-my-booking.",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Pay booked court with Nytt konto-/kreditkort",
+  "description": "",
+  "id": "as-a-customer-i-want-to-be-able-choose-different-payment-methods-for-my-booking.;pay-booked-court-with-nytt-konto-/kreditkort",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "I am logged in to Matchi",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "have chosen a sport hall",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "chosen a specific court",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "chosen a specific time",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I choose Nytt konto-/kreditkort",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "I enter card details: \"2223000048410010\", \"Mjukvaru Testare\", \"10\", \"2020\", \"737\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I can finalize my booking",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_am_logged_in_to_Matchi()"
+});
+formatter.result({
+  "duration": 13209296828,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.have_chosen_a_sport_hall()"
+});
+formatter.result({
+  "duration": 5293009650,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.chosen_a_specific_court()"
+});
+formatter.result({
+  "duration": 58272,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.chosen_a_specific_time()"
+});
+formatter.result({
+  "duration": 353432389,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_choose_Nytt_konto_kreditkort()"
+});
+formatter.result({
+  "duration": 1658231120,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2223000048410010",
+      "offset": 23
+    },
+    {
+      "val": "Mjukvaru Testare",
+      "offset": 43
+    },
+    {
+      "val": "10",
+      "offset": 63
+    },
+    {
+      "val": "2020",
+      "offset": 69
+    },
+    {
+      "val": "737",
+      "offset": 77
+    }
+  ],
+  "location": "MatchiTestSteps.i_enter_card_details(String,String,String,String,String)"
+});
+formatter.result({
+  "duration": 6483773393,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_can_finalize_my_booking()"
+});
+formatter.result({
+  "duration": 4486138759,
+  "status": "passed"
+});
+formatter.uri("ProjektMatchi/ProjektMatchi/SearchMatchiSportHall.feature");
+formatter.feature({
+  "line": 1,
+  "name": "As a customer i want to search for a sport hall so that i can book it",
+  "description": "",
+  "id": "as-a-customer-i-want-to-search-for-a-sport-hall-so-that-i-can-book-it",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Search for Sport Hall",
+  "description": "",
+  "id": "as-a-customer-i-want-to-search-for-a-sport-hall-so-that-i-can-book-it;search-for-sport-hall",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "I am logged in to Matchi",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I enter the \"Hönö\" of a Sport Hall",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "As I click the name of the Sport Hall",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "Can choose the clicked Sport Hall for booking",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_am_logged_in_to_Matchi()"
+});
+formatter.result({
+  "duration": 13308450196,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Hönö",
+      "offset": 13
+    }
+  ],
+  "location": "MatchiTestSteps.i_enter_the_of_a_Sport_Hall(String)"
+});
+formatter.result({
+  "duration": 8962260001,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.as_I_click_the_name_of_the_Sport_Hall()"
+});
+formatter.result({
+  "duration": 6753128961,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.can_choose_the_clicked_Sport_Hall_for_booking()"
+});
+formatter.result({
+  "duration": 39806,
+  "status": "passed"
+});
+formatter.uri("ProjektMatchi/ProjektMatchi/SelectActivityDate.feature");
+formatter.feature({
+  "line": 1,
+  "name": "As a customer I want to select a date and a time for the sport activity",
+  "description": "",
+  "id": "as-a-customer-i-want-to-select-a-date-and-a-time-for-the-sport-activity",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "search for a sportshall and check for available times",
+  "description": "",
+  "id": "as-a-customer-i-want-to-select-a-date-and-a-time-for-the-sport-activity;search-for-a-sportshall-and-check-for-available-times",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "I am logged in to Matchi",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I select boka",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I search for \"Höno\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "I click a time",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I see the available courts for that time",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_am_logged_in_to_Matchi()"
+});
+formatter.result({
+  "duration": 14042744450,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_select_boka()"
+});
+formatter.result({
+  "duration": 3080182007,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Höno",
+      "offset": 14
+    }
+  ],
+  "location": "MatchiTestSteps.i_search_for(String)"
+});
+formatter.result({
+  "duration": 875178049,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_click_a_time()"
+});
+formatter.result({
+  "duration": 964679947,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MatchiTestSteps.i_see_the_available_courts_for_that_time()"
+});
+formatter.result({
+  "duration": 1979350230,
+  "status": "passed"
+});
 });
